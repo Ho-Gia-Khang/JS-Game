80135d4bc0dd3685ceb80e357e0b5f59b986cc96
KinJi02 <IamKinji23@gmail.com>
2023-07-03 20:46:28 +0700

    failed to render the image since the exported image was at the wrong scale

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const ctx = canvas.getContext("2d");
 
 ctx.fillStyle = "white";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+const image = new Image();
+image.src = "../images/GameMap.png";
+
+image.onload = () => {
+    ctx.drawImage(image, 0, 0);
+};
