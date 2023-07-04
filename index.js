da23ccf889d068fa1402f0015f85bbc228d26706
Ho-Gia-Khang <hogiakhanglatoi@gmail.com>
2023-07-04 15:02:01 +0700

    added movement mechanic
    player now can move around the map, but player now can move freely

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ const ctx = canvas.getContext("2d");
 ctx.fillStyle = "white";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+class Sprite {
+    constructor({ position, image }) {
+        this.position = position;
+        this.image = image;
+    }
+
+    draw() {
+        ctx.drawImage(this.image, this.position.x, this.position.y);
+    }
+}
+
 // player character initializing
 const playerImage = new Image();
 playerImage.src = "../images/playerDown.png";
@@ -11,10 +22,33 @@ playerImage.src = "../images/playerDown.png";
 // background initializing
 const backgroundImage = new Image();
 backgroundImage.src = "../images/GameMap.png";
+const background = new Sprite({
+    position: {
+        x: -930,
+        y: -490,
+    },
+    image: backgroundImage,
+});
+
+const keys = {
+    w: {
+        pressed: false,
+    },
+    a: {
+        pressed: false,
+    },
+    s: {
+        pressed: false,
+    },
+    d: {
+        pressed: false,
+    },
+};
 
 // rendering
-backgroundImage.onload = () => {
-    ctx.drawImage(backgroundImage, -930, -490);
+animate = () => {
+    window.requestAnimationFrame(animate);
+    background.draw();
     ctx.drawImage(
         playerImage,
         0,
@@ -26,4 +60,55 @@ backgroundImage.onload = () => {
         playerImage.width / 4,
         playerImage.height
     );
+
+    if (keys.w.pressed && lastKey === "w") {
+        background.position.y += 4;
+    } else if (keys.s.pressed && lastKey === "s") {
+        background.position.y -= 4;
+    } else if (keys.a.pressed && lastKey === "a") {
+        background.position.x += 4;
+    } else if (keys.d.pressed && lastKey === "d") {
+        background.position.x -= 4;
+    }
 };
+
+animate();
+
+let lastKey = "";
+window.addEventListener("keydown", (e) => {
+    switch (e.key) {
+        case "w":
+            keys.w.pressed = true;
+            lastKey = "w";
+            break;
+        case "a":
+            keys.a.pressed = true;
+            lastKey = "a";
+            break;
+        case "s":
+            keys.s.pressed = true;
+            lastKey = "s";
+            break;
+        case "d":
+            keys.d.pressed = true;
+            lastKey = "d";
+            break;
+    }
+});
+
+window.addEventListener("keyup", (e) => {
+    switch (e.key) {
+        case "w":
+            keys.w.pressed = false;
+            break;
+        case "a":
+            keys.a.pressed = false;
+            break;
+        case "s":
+            keys.s.pressed = false;
+            break;
+        case "d":
+            keys.d.pressed = false;
+            break;
+    }
+});
