940730c3d736ab31398819154bc5975ddf50faa0
Ho-Gia-Khang <hogiakhanglatoi@gmail.com>
2023-07-13 10:44:23 +0700

    added some offset for the player movement and adjust the foreground_above layer

--- utilities.js
+++ utilities.js
@@ -1,8 +1,13 @@
 isRectangularCollision = ({ rectangle1, rectangle2 }) => {
+    const offset_y = 40;
+    const offset_x = 8;
     return (
-        rectangle1.position.x + rectangle1.width >= rectangle2.position.x &&
-        rectangle1.position.x <= rectangle2.position.x + rectangle2.width &&
-        rectangle1.position.y <= rectangle2.position.y + rectangle2.height &&
+        rectangle1.position.x + rectangle1.width >=
+            rectangle2.position.x + offset_x &&
+        rectangle1.position.x + offset_x <=
+            rectangle2.position.x + rectangle2.width &&
+        rectangle1.position.y + offset_y <=
+            rectangle2.position.y + rectangle2.height &&
         rectangle1.position.y + rectangle1.height >= rectangle2.position.y
     );
 };
